2a925ae9479fb9500ff9e614c9f591c724a6849c
James Bergstra <james.bergstra@gmail.com>
2011-08-05 14:08:09 -0400

    pf

--- exercises/particle_filter.py
+++ exercises/particle_filter.py
@@ -4,3 +4,29 @@ class ParticleFilter(unittest.TestCase):
     """
     """
 
+    def setUp(self):
+        """
+        Configure a particle-filtering problem.
+        """
+
+        # set up the stochastic dynamical system that we're going to track
+        
+        # allocate particles
+
+        # insert filtering equations here
+
+    def test_basic_tracking(self):
+        """
+        Writeme
+        """
+
+    def test_resampling_heuristics(self):
+        """
+        Writeme
+        """
+
+    def test_number_of_particles(self):
+        """
+        Writeme
+        """
+
